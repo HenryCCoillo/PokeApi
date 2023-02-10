fcfc81864767caa59bd77a82bcac1d9417e2beb7
Henry Ccoillo <henryccoillo11@gmail.com>
2023-02-10 13:37:14 -0500

    Configuracion para Railway

--- LoginProject/settings.py
+++ LoginProject/settings.py
@@ -66,6 +66,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -167,3 +168,5 @@ AUTH_USER_MODEL = 'users.User'
 import os
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
