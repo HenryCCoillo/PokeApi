8b1668dc392a05520e1fc478c09358361d401e9a
JhohanJC <111875@unsaac.edu.pe>
2023-02-10 20:40:22 -0500

    ADD database extractor of pokeapi

--- dbextractor.py
+++ dbextractor.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import pandas as pd
-##  ______________________ todos los pokemones __________________________ #
+##  ______________________ Extracción todos los pokemones __________________________ #
 # URL de la API de PokeAPI
 url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=2000"
 # url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
@@ -20,17 +20,19 @@ if response.status_code == 200:
             pokemon_id = data_pokemon['id']
             poke_pokemon['pokemon_id']=pokemon_id
 
-            ## nombre del pokemon
+            ## Nombre del pokemon
             name_pokemon = data_pokemon['name']
             poke_pokemon['name']=name_pokemon
 
-            ## altura del pokemon
+            ## Altura del pokemon
             height_pokemon = data_pokemon['height']
 
-            ## peso del pokemon
+            ## Peso del pokemon
             weight_pokemon = data_pokemon['weight']
 
-            ## descripcion del pokemon
+            ## Descripcion del pokemon
+            # Altura: decimetros a metros
+            # Peso: hectogramos a kilogramos
             descripction = f'La altura del pokemon {round(float(height_pokemon)*0.1,2)} m y su peso es {round(float(weight_pokemon)*0.1,2)} kg'
             poke_pokemon['descripction']=descripction
 
@@ -38,7 +40,7 @@ if response.status_code == 200:
             for k in data_pokemon['stats']:
                 poke_pokemon[k['stat']['name']]=k['base_stat']
 
-            ## imagen del pokemon
+            ## Imagen del pokemon
             url_imagen= data_pokemon['sprites']['other']
             pokemon_image=url_imagen['official-artwork']['front_default']
             poke_pokemon['img']=pokemon_image
@@ -52,5 +54,5 @@ if response.status_code == 200:
 else:
     print("Falló ...")
 df = pd.read_json('dbpokemon.json', orient = 'registros')
-# guardar en formato csv
+# Guardar en formato csv
 df.to_csv('dbpokemon.csv', index=False)
